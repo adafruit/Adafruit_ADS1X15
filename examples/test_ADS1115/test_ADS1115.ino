7db1cfd6768138b87104650e8a1a7e26e1318ec0
Dennis <soligen2010@gmail.com>
2016-02-12 18:49:30 -0500

    Added circuit info to test sketch comments
    Description of the components and connections needed added to the test
    sketch

--- examples/test_ADS1115/test_ADS1115.ino
+++ examples/test_ADS1115/test_ADS1115.ino
@@ -1,8 +1,45 @@
+
+/*
+This sketch tests the library, outputting the values ready to Serial.
+Only one gain setting is tested at a time.  If the gain is changed to
+GAIN_SIXTEEN then some of the read voltagees exceed the gain range and should
+read as the max value.  With the resistors below all other gains should 
+produce correct results (within a reasonable tolerance). Note that reading
+A0 (GND) may produce a slightly negative voltage
+
+Sketch and circuit by soligen2010, Feb 12, 2016
+
+Below are the components and connections for the test circuit for the test:
+
+Components:
+
+ADS1115
+D1 1N4004 (Cathode to A0, Anode to Pin 3 for arduino - choose different pin for ESP8266)
+R1 3k3    (VDD to A0)
+R2 240R   (A0 to A1)
+R3 1K     (A1 to A2)
+R4 1K     (A2 to A3)
+
+Connections:
+
+VDD ---- +5, R1 (VDD to A0)
+GND ---- Gound
+SCL ---- Arduino Uno A5 or ESP8266 Pin 5
+SDA ---- Arduino Uno A4 or ESP8266 Pin 4
+ADDR --- GND
+ALRT --- Digital Pin 2
+A0 ----- R1, D1(Cathode to A0, Anode to Pin 3), R2
+A1 ----- R2, R3
+A2 ----- R3, R4
+A3 ----- R4, GND
+
+*/
+
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
- Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-//Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+  Adafruit_ADS1115 ads;     // Use this for the 16-bit version 
+//Adafruit_ADS1015 ads;     // Use this for the 12-bit version 
 
 float   multiplier;
 
@@ -24,26 +61,27 @@ void setup(void)
   // Setting these values incorrectly may destroy your ADC!
   //                                                                ADS1015  ADS1115
   //                                                                -------  -------
-   ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+     ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
   // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
   // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
   
-  //ads.begin(1,0);   // for ESP8266  SDA, SCL can be specified
+  //ads.begin(1,0);                // for ESP8266  SDA, SCL can be specified
   ads.begin();
   
-  multiplier = ads.voltsPerBit()*1000.0F;    /* Sets the millivolts per bit */
+  multiplier = ads.voltsPerBit()*1000.0F;           // Gets the millivolts per bit 
   
   /* Use this to set data rate for the 12-bit version (optional)*/
-  //ads.setSPS(ADS1015_DR_3300SPS);      // for ADS1015 fastest samples per second is 3300 (default is 1600)
+  //ads.setSPS(ADS1015_DR_3300SPS);                 // for ADS1015 fastest samples per second is 3300 (default is 1600)
   
   /* Use this to set data rate for the 16-bit version (optional)*/
-  ads.setSPS(ADS1115_DR_8SPS);      // for ADS1115 fastest samples per second is 860 (default is 128)
+  ads.setSPS(ADS1115_DR_8SPS);                      // for ADS1115 fastest samples per second is 860 (default is 128)
 
-  ads.readADC_Differential_0_1();  //in case chip was previously in contuous mode, take out of continuous
-  ads.waitForConversion();                          //delay to ensure any last remaining conversion completes
+  ads.readADC_Differential_0_1();                   // in case chip was previously in contuous mode, take out of continuous
+  ads.waitForConversion();                          // delay to ensure any last remaining conversion completes
+                                                    // needed becasue if formerly was in continuous, 2 conversions need to complete
   
   // Run test for each SPS
   
@@ -126,20 +164,22 @@ void runTest()
   results2_3 = ads.readADC_SingleEnded(3);
   Serial.println(micros() - sampleTime);
 
-  int16_t compThresh = ads.readADC_SingleEnded(0) + 200;
-
+  // Test comparator mode
+  
+  int16_t compThresh = ads.readADC_SingleEnded(0) + 200;       // Set the threshold slighly above A0
+ 
   ads.startComparator_SingleEnded(0,compThresh);
   Serial.print("Comparator started.  Alert Pin (should be 1) = ");Serial.println(digitalRead(alertPin));
-  digitalWrite(raiseVoltagePin,HIGH);                // raise the voltage
-  do {delay(0);} while (digitalRead(alertPin) == HIGH);
+  digitalWrite(raiseVoltagePin,HIGH);                          // raise the voltage.  This feeds 5V via D1 into A0, bypassing R1
+  do {delay(0);} while (digitalRead(alertPin) == HIGH);        // Wait until alert signaled
   Serial.print("Comparator triggered.  Alert Pin (should be 0) = ");Serial.println(digitalRead(alertPin));
-  digitalWrite(raiseVoltagePin,LOW);                // lower voltage back down
+  digitalWrite(raiseVoltagePin,LOW);                           // lower voltage back down
   delay(1);
-  results0_3 = ads.getLastConversionResults();           // reading conversion clears the latch
-  results0_1 = ads.readADC_SingleEnded(0);               //stops the contuous reading of the comparator mode
-  ads.waitForConversion();                                   // delay to be sure last conversion ends. changing
-                                                         // from continuous to single the read may pick up the last continuous
-                                                         // instead of the newly initiated single read              
+  results0_3 = ads.getLastConversionResults();                 // reading conversion clears the latch
+  results0_1 = ads.readADC_SingleEnded(0);                     // stops the contuous reading of the comparator mode
+  ads.waitForConversion();                                     // delay to be sure last conversion ends. changing
+                                                               // from continuous to single the read may pick up the last continuous
+                                                               // instead of the newly initiated single read              
   
   Serial.println();
 
