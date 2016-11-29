f1e29de8350a11fea40eac42dcab9ecc8f387994
Dennis <soligen2010@gmail.com>
2016-11-28 22:39:04 -0500

    Corrented comment

--- Adafruit_ADS1015.cpp
+++ Adafruit_ADS1015.cpp
@@ -523,7 +523,7 @@ void Adafruit_ADS1015::startContinuous_SingleEnded(uint8_t channel)
 /**************************************************************************/
 void Adafruit_ADS1015::startContinuous_Differential(adsDiffMux_t regConfigDiffMUX)
 {
-  // Initial single ended non-contunuous read primes the conversion buffer with a valid reading
+  // Initial Differential non-contunuous read primes the conversion buffer with a valid reading
   // so that the initial interrupts produced a correct result instead of a left over 
   // conversion result from previous operations.
   int16_t primingRead = readADC_Differential(regConfigDiffMUX); 
