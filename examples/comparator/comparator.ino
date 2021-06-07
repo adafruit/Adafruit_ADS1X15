c551c79ef874ddff08d2e8d7e66f17fc29a8ff88
caternuson <caternuson@gmail.com>
2021-06-07 11:26:21 -0700

    check begin and park if failed

--- examples/comparator/comparator.ino
+++ examples/comparator/comparator.ino
@@ -3,15 +3,15 @@
 // Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
 
-void setup(void) 
+void setup(void)
 {
   Serial.begin(9600);
   Serial.println("Hello!");
-  
+
   Serial.println("Single-ended readings from AIN0 with >3.0V comparator");
   Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
   Serial.println("Comparator Threshold: 1000 (3.000V)");
-  
+
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
@@ -24,20 +24,22 @@ void setup(void)
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  
-  ads.begin();
-  
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
   // Setup 3V comparator on channel 0
   ads.startComparator_SingleEnded(0, 1000);
 }
 
-void loop(void) 
+void loop(void)
 {
   int16_t adc0;
 
   // Comparator will only de-assert after a read
   adc0 = ads.getLastConversionResults();
   Serial.print("AIN0: "); Serial.println(adc0);
-  
+
   delay(100);
 }
--- examples/differential/differential.ino
+++ examples/differential/differential.ino
@@ -7,10 +7,10 @@ void setup(void)
 {
   Serial.begin(9600);
   Serial.println("Hello!");
-  
+
   Serial.println("Getting differential reading from AIN0 (P) and AIN1 (N)");
   Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-  
+
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
@@ -23,20 +23,22 @@ void setup(void)
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  
-  ads.begin();
-}
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }}
 
 void loop(void)
 {
   int16_t results;
-  
+
   /* Be sure to update this value based on the IC and the gain settings! */
   float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
   //float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
 
-  results = ads.readADC_Differential_0_1();  
-    
+  results = ads.readADC_Differential_0_1();
+
   Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(results * multiplier); Serial.println("mV)");
 
   delay(1000);
