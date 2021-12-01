ba62a2998bb1376a5f6eed15467685bf590c8b92
Doxygen CI <ci-arduino@invalid>
2021-12-01 17:02:34 +0000

    Deploy docs to GitHub Pages from commit da24495248
    Commit: da24495248886e9dfce5f5cec5359797fd586998
    GitHub Actions run: 1526857075

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -16,6 +16,8 @@ var searchData=
   ['rate_5fads1115_5f860sps',['RATE_ADS1115_860SPS',['../_adafruit___a_d_s1_x15_8h.html#afb94073e4b65ae3fd163240c4930c2ee',1,'Adafruit_ADS1X15.h']]],
   ['rate_5fads1115_5f8sps',['RATE_ADS1115_8SPS',['../_adafruit___a_d_s1_x15_8h.html#ab6f372e80cf39e156a0ed31100dcb33b',1,'Adafruit_ADS1X15.h']]],
   ['readadc_5fdifferential_5f0_5f1',['readADC_Differential_0_1',['../class_adafruit___a_d_s1_x15.html#a9451025a1d57b0d01eed5e5cbb3d9249',1,'Adafruit_ADS1X15']]],
+  ['readadc_5fdifferential_5f0_5f3',['readADC_Differential_0_3',['../class_adafruit___a_d_s1_x15.html#a07016074440ba793636633da32a3f30d',1,'Adafruit_ADS1X15']]],
+  ['readadc_5fdifferential_5f1_5f3',['readADC_Differential_1_3',['../class_adafruit___a_d_s1_x15.html#a7b727524444305319aa60c21b1a58239',1,'Adafruit_ADS1X15']]],
   ['readadc_5fdifferential_5f2_5f3',['readADC_Differential_2_3',['../class_adafruit___a_d_s1_x15.html#abbc2117832a544d26fe50129a53dcf33',1,'Adafruit_ADS1X15']]],
   ['readadc_5fsingleended',['readADC_SingleEnded',['../class_adafruit___a_d_s1_x15.html#a9048c868e4904071b83c74b224918fc8',1,'Adafruit_ADS1X15']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['readadc_5fdifferential_5f0_5f1',['readADC_Differential_0_1',['../class_adafruit___a_d_s1_x15.html#a9451025a1d57b0d01eed5e5cbb3d9249',1,'Adafruit_ADS1X15']]],
+  ['readadc_5fdifferential_5f0_5f3',['readADC_Differential_0_3',['../class_adafruit___a_d_s1_x15.html#a07016074440ba793636633da32a3f30d',1,'Adafruit_ADS1X15']]],
+  ['readadc_5fdifferential_5f1_5f3',['readADC_Differential_1_3',['../class_adafruit___a_d_s1_x15.html#a7b727524444305319aa60c21b1a58239',1,'Adafruit_ADS1X15']]],
   ['readadc_5fdifferential_5f2_5f3',['readADC_Differential_2_3',['../class_adafruit___a_d_s1_x15.html#abbc2117832a544d26fe50129a53dcf33',1,'Adafruit_ADS1X15']]],
   ['readadc_5fsingleended',['readADC_SingleEnded',['../class_adafruit___a_d_s1_x15.html#a9048c868e4904071b83c74b224918fc8',1,'Adafruit_ADS1X15']]]
 ];
