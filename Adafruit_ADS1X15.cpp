2d315b5dca57e2b11da0a1c0f4451115f2cb85fe
Carter Nelson <caternuson@gmail.com>
2024-02-02 11:44:11 -0800

    Revert "Fix doc string and millivolts value in examples"

--- Adafruit_ADS1X15.cpp
+++ Adafruit_ADS1X15.cpp
@@ -276,7 +276,7 @@ int16_t Adafruit_ADS1X15::getLastConversionResults() {
 
 /**************************************************************************/
 /*!
-    @brief  Compute volts for the given raw counts.
+    @brief  Returns true if conversion is complete, false otherwise.
 
     @param counts the ADC reading in raw counts
 
